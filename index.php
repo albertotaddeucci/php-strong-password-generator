6c959f2d8ae24291a03fc6648c4362ca096eb3a1
alberto <alberto.taddeucci@gmail.com>
2024-03-19 15:55:39 +0100

    milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,57 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+if (isset($_GET['pwLength'])) {
+
+    $array = [];
+
+    function chrRandom()
+    {
+        $chrRandom = chr(64 + rand(1, 26));
+        return $chrRandom;
+    };
+    function chrRandomMin()
+    {
+        $chrRandomMin = strtolower(chr(64 + rand(1, 26)));
+        return $chrRandomMin;
+    }
+    function numRandom()
+    {
+        $numRandom = rand(0, 9);
+        return $numRandom;
+    }
+    function chrRandomSpec()
+    {
+        $chrRandomSpec = chr(32 + rand(1, 15));
+        return $chrRandomSpec;
+    };
+
+
+
+    for ($i = 0; $i < $_GET['pwLength']; $i++) {
+        $num = rand(1, 4);
+
+        if ($num == 1) {
+            $array[] = chrRandom();
+        } else if ($num == 2) {
+            $array[] = chrRandomMin();
+        } else if ($num == 3) {
+            $array[] = numRandom();
+        } else if ($num == 4) {
+            $array[] = chrRandomSpec();
+        }
+    };
+
+    // var_dump($array);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,8 +66,36 @@
 </head>
 
 <body>
+    <div class="container pt-4">
 
-    <h1>Password Generator</h1>
+        <form action="" method="GET">
+
+            <h1 class="py-4">Password Generator</h1>
+
+            <div class="mb-3">
+                <label for="pw-length" class="form-label">Inserire lunghezza password</label>
+                <input type="number" name="pwLength" class="form-control" id="pw-length" placeholder="Lunghezza">
+            </div>
+
+            <input type="submit" class="btn btn-danger">
+
+
+        </form>
+
+        <h2 class="pt-5 pb-2">Password</h2>
+        <p>
+            <?php
+
+            foreach ($array as $item) {
+                echo $item;
+            }
+
+            ?>
+        </p>
+    </div>
+
+
+
 
 
     <!-- bootstrap -->
